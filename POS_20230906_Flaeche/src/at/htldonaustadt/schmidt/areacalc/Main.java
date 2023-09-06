87e08a1512d6f98b036ece8f0ababa0a3414bb27
Julian-AT <julian.schmidt1005@gmail.com>
2023-09-06 10:30:05 +0200

    remove boilerplate comment

--- POS_20230906_Flaeche/src/at/htldonaustadt/schmidt/areacalc/Main.java
+++ POS_20230906_Flaeche/src/at/htldonaustadt/schmidt/areacalc/Main.java
@@ -1,9 +1,6 @@
 package at.htldonaustadt.schmidt.areacalc;
 
 import java.util.Scanner;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // fetch circle data from the console

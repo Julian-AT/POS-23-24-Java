0024cb277f94aeca1ce5f8253a6d301b9206a170
Julian-AT <julian.schmidt1005@gmail.com>
2023-09-07 09:59:27 +0200

    add demo file reader
    Demo File Reader for file parsing

---
+++ POS_20230907_FileReader/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // Demo for File Reading
+        try (Scanner scanner = new Scanner(new File("input.txt")))  {
+            while (scanner.hasNext()) {
+                if (scanner.hasNextInt()) {
+                    System.out.printf(String.format("%d is a Integer.%n", scanner.nextInt()));
+                }
+                else if (scanner.hasNextDouble()) {
+                    System.out.printf(String.format("%.2f is a Double.%n", scanner.nextDouble()));
+                }
+                else {
+                    System.out.printf(String.format("%s is a String.%n", scanner.next()));
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            // Print localized error message + Stack Trace
+            System.out.println(e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+
+    }
+}
